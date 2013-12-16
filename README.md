69424f89caafd3b5d2289d6cff89634187c56d48
Jimmy Royer <jimleroyer@gmail.com>
2013-12-16 18:58:25 -0500

    Added RAR and ZIP extraction capabilities along with automatic subtitles download for all contained videos.

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ A script that downloads subtitles from opensubtitles automatically for a given l
 
 Requirements
 ============
-The script requires the python-opensubtitles module which can be found at: https://github.com/agonzalezro/python-opensubtitles
+
+  * Requires the *python-opensubtitles* module which can be found at: https://github.com/agonzalezro/python-opensubtitles
+  * Requires the *rarfile* module which you can install with `pip install rarfile`
+  * Requires the *unrar* executable for your platform if you want to use the RAR extraction capabilities
 
 Usage
 =====
 First, edit the osdown.cfg file and add your username and password in the Login section.
 You can also select your default language id (currently set to heb - for hebrew)
 When running 'python osdown.py <filepath>' the script will download the first subtitle found.
-
--- osdown.py
+++ osdown.py
@@ -1,19 +1,60 @@
 #!/usr/bin/env python
+
+########## Imports
+
 from pythonopensubtitles.opensubtitles import OpenSubtitles
 from pythonopensubtitles.utils import File
+
 import ConfigParser
-import urllib
-from tempfile import mktemp
-import gzip
 from numbers import Number
+
+import gzip
+import rarfile
+import zipfile
+
+from tempfile import mktemp
 import os
 from os import path
+import urllib
 
+
+########## Constants
+
+RAR_EXT = [ ".rar" ]
+ZIP_EXT = [ ".zip" ]
+
+COMPRESSED_EXTS = RAR_EXT + ZIP_EXT
+VID_EXTS = [ ".avi", ".h264", ".hdmov", ".mkv", ".mov", ".mp4", ".ogv", ".xvid" ]
+
+
+########## Configuration and setup
 
 config = ConfigParser.RawConfigParser()
 config.read(os.path.realpath(__file__)[:-2] + 'cfg')
 osmgr = OpenSubtitles()
 osmgr.login(config.get('Login', 'user'), config.get('Login', 'pass'))
+
+
+########## Main functions
+
+def download_subtitle(subtitle):
+    print 'Downloading %s...' % subtitle['SubFileName']
+    tempfile = mktemp()
+    urllib.urlretrieve(subtitle['SubDownloadLink'], tempfile)
+    with gzip.open(tempfile, 'rb') as gz, open(subtitle['SubFileName'], 'wb') as f:
+        f.write(gz.read())
+    os.remove(tempfile)
+
+
+def extract_videos(compressed_file):
+    #with open(compressed_file, "rb") as fh:
+    # Select the right API to extract the files out of the archive.
+    klass = extract_dict[file_ext(compressed_file)]
+    archive = klass(compressed_file)
+    videos = [video for video in archive.namelist() if file_ext(video) in VID_EXTS]
+    print "Extracting videos (using %s) %s..." % (str(klass), ", ".join(videos))
+    map(archive.extract, videos, ["."] * len(videos))
+    return videos
 
 
 def find_subtitles(path, langid='all'):
@@ -30,28 +71,6 @@ def find_subtitles(path, langid='all'):
     return data
 
 
-def download_subtitle(subtitle):
-    print 'Downloading %s...' % subtitle['SubFileName']
-    tempfile = mktemp()
-    urllib.urlretrieve(subtitle['SubDownloadLink'], tempfile)
-    with gzip.open(tempfile, 'rb') as gz, open(subtitle['SubFileName'], 'wb') as f:
-        f.write(gz.read())
-    os.remove(tempfile)
-
-
-def rename_subtitle(vid_filename, sub_filename):
-    print "Renaming subtitle %s..." % sub_filename
-    vid_name = path.splitext(vid_filename)[0]
-    sub_ext = path.splitext(sub_filename)[1]
-    name = vid_name + sub_ext
-    os.rename(sub_filename, name)
-
-
-def usage():
-    import sys
-    print "%s <filename> [<langid>]" % sys.argv[0]
-
-
 def main():
     import sys
     if len(sys.argv) < 2:
@@ -61,9 +80,47 @@ def main():
     langid = config.get('Language', 'langid')
     if len(sys.argv) >= 3:
         langid = sys.argv[2]
+    if file_ext(filename) in COMPRESSED_EXTS:
+        videos = extract_videos(filename)
+        map(process_subtitle, videos, [langid] * len(videos))
+    else:
+        process_subtitle(filename, langid)
+
+
+def process_subtitle(filename, langid):
     subtitle = find_subtitles(filename, langid)[0]
     download_subtitle(subtitle)
     rename_subtitle(filename, subtitle['SubFileName'])
+
+
+def rename_subtitle(vid_filename, sub_filename):
+    print "Renaming subtitle %s..." % sub_filename
+    vid_name = file_name(vid_filename)
+    sub_ext = file_ext(sub_filename)
+    name = vid_name + sub_ext
+    os.rename(sub_filename, name)
+
+
+########## General Utilities
+
+# Compressed files extraction libraries
+extract_class = [ rarfile.RarFile, zipfile.ZipFile ]
+extract_dict = dict(zip(COMPRESSED_EXTS, extract_class))
+
+
+def file_ext(filename):
+    return path.splitext(filename)[1]
+
+
+def file_name(filename):
+    return path.splitext(filename)[0]
+
+
+# Command-line Interface
+
+def usage():
+    import sys
+    print "%s <filename> [<langid>]" % sys.argv[0]
 
 
 if __name__ == '__main__':
